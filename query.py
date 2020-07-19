4ee07492d38f9287d0e93e5a403b224fe8d8b674
tsa87 <tsa87@sfu.ca>
2020-07-19 11:04:40 -0700

    Fix SQL query spacing bug

--- query.py
+++ query.py
@@ -3,12 +3,12 @@ from google.cloud import bigquery
 table_prefix = "bigquery-public-data.gnomAD.v2_1_1_exomes__chr"
 
 _GET_MUTATION_COUNT_QUERY = (
-    'SELECT count(1) AS num_mutations'
-    'FROM {TABLE_NAME} AS T, T.alternate_bases AS ALT'
+    'SELECT count(1) AS num_mutations '
+    'FROM {TABLE_NAME} AS T, T.alternate_bases AS ALT '
     'WHERE start_position >= {START_POSITION} AND start_position <= {END_POSITION} AND ALT.AF <=0.01')
 
 _GET_MAX_START_POSITION_QUERY = (
-    'SELECT MAX(start_position)'
+    'SELECT MAX(start_position) '
     'FROM {TABLE_NAME}'
 )
 
@@ -16,11 +16,8 @@ class BigQueryCaller:
     """ Contains the BigQuery API Client and calling mechanism """
 
     def __init__(self, client=None, num_retries=5):
-    """ Initialize `BigQueryCaller` object.
-    Args:
-        client: Bigquery API client object.
-        num_retries: Re-attempt limit for TimeoutErrors in queries.
-    """
+
+
         if client is None:
             self._client = bigquery.Client()
         else:
@@ -38,13 +35,13 @@ class BigQueryCaller:
                 iterator = query_job.result(timeout=300)
             except TimeoutError as e:
                 print('Time out waiting for query: %s', query)
-            if num_retries < self._num_retries:
-                num_retries += 1
-                time.sleep(90)
+                if num_retries < self._num_retries:
+                    num_retries += 1
+                    time.sleep(90)
+                else:
+                    raise e
             else:
-                raise e
-          else:
-              break
+                break
         result = []
         for i in iterator:
             result.append(str(i.values()[0]))
