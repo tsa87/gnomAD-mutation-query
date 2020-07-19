f8aeea5b34c4c82d44f701baf0e442163906e144
tsa87 <tsa87@sfu.ca>
2020-07-19 15:31:56 -0700

    Add function that takes in table_name, start_pos, end_pos and interval for generalized usage

--- query.py
+++ query.py
@@ -1,12 +1,13 @@
 from google.cloud import bigquery
 from hurry.filesize import size
+import pandas as pd
 
 table_prefix = "bigquery-public-data.gnomAD.v2_1_1_exomes__chr"
 
 _GET_MUTATION_COUNT_QUERY = (
     'SELECT count(1) AS num_mutations '
     'FROM {TABLE_NAME} AS T, T.alternate_bases AS ALT '
-    'WHERE start_position >= {START_POSITION} AND start_position <= {END_POSITION} AND ALT.AF <=0.01')
+    'WHERE start_position >= {START_POSITION} AND start_position < {END_POSITION} AND ALT.AF <=0.01')
 
 _GET_MAX_START_POSITION_QUERY = (
     'SELECT MAX(start_position) '
@@ -25,14 +26,42 @@ class BigQueryCaller:
 
         self._num_retries = num_retries
 
-    def _est_query_cost(self, query):
+
+    def run_query(self, table_name, query_template, start_position, end_position, interval):
+        output = pd.DataFrame()
+        
+        for i in range(start_position, end_position-interval, interval):
+            query = query_template.format(
+                TABLE_NAME=table_name,
+                START_POSITION=i,
+                END_POSITION=i+interval
+            )
+            cost = self._get_query_cost(query)
+            result = int(self._get_query_result(query)[0])
+            
+            row = {
+                'start_position': i, 
+                'end_position': i+interval,
+                'mutation_count': result,
+                'bytes_processed': cost }
+    
+            output = output.append(row, ignore_index=True)
+            
+        columns = ['start_position', 'end_position', 'mutation_count', 'bytes_processed']
+        output = output.reindex(columns=columns)
+            
+        return output
+            
+        
+    def _get_query_cost(self, query):
         job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
         query_job = self._client.query(query, job_config=job_config)
         
         return query_job.total_bytes_processed
 
+
     # https://github.com/googlegenomics/gcp-variant-transforms/blob/master/gcp_variant_transforms/libs/partitioning.py
-    def _run_query(self, query):
+    def _get_query_result(self, query):
         query_job = self._client.query(query)
 
         num_retries = 0
@@ -52,25 +81,12 @@ class BigQueryCaller:
         for i in iterator:
             result.append(str(i.values()[0]))
 
-        total_bytes_billed = query_job.total_bytes_billed
-        print(total_bytes_billed)
-
         return result
 
 
 caller = BigQueryCaller()
 table_name = table_prefix + "1"
 
-query = _GET_MAX_START_POSITION_QUERY.format(TABLE_NAME=table_name)
-print(size(caller._est_query_cost(query)))
-#max_start_position = caller._run_query(query)[0]
+df = caller.run_query(table_name, _GET_MUTATION_COUNT_QUERY, 0, 500000, 100000)
+df.to_csv("output.csv")
 
-start_position = 0
-
-query = _GET_MUTATION_COUNT_QUERY.format(
-    TABLE_NAME=table_name,
-    START_POSITION=start_position,
-    END_POSITION=start_position+100000
-)
-
-#num_mutations = caller._run_query(query)[0]
